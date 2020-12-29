fb5c942d326394aece6726e226efb6f59f56dfce
Joaquim Silva <joaquim.diego8@gmail.com>
2020-12-29 13:03:38 -0300

    API atualizada

---
+++ Java-Pratica/JAVA/pratica/pratica.java
@@ -0,0 +1,7 @@
+package io.github.jiangdequan;
+
+public class pratica {
+public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    
+}
--- picpayclone/src/main/java/br/com/dio/picpayclone/Model/EntidadeBase.java
+++
@@ -1,9 +0,0 @@
-package br.com.dio.picpayclone.Model;
-
-import java.util.function.LongBinaryOperator;
-
-public class EntidadeBase {
-    private Bigint Id;
-
-    
-}
