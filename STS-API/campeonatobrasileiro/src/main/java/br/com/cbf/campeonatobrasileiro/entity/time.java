6c3cf7fdd2f06cf4947a39b353a107dff27fa5ac
Joaquim Silva <joaquim.diego8@gmail.com>
2020-12-30 11:52:57 -0300

    API atualizada

--- STS-API/campeonatobrasileiro/src/main/java/br/com/cbf/campeonatobrasileiro/entity/time.java
+++ STS-API/campeonatobrasileiro/src/main/java/br/com/cbf/campeonatobrasileiro/entity/time.java
@@ -5,18 +5,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+
 
 import lombok.Data;
 
+
 @Entity
 @Data
-@Table(name = "times")
 
 public class time {
-    
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
      private Integer id;
      @Column(length = 20)
      private String nome;
@@ -24,4 +24,6 @@ public class time {
      private String sigla;
      @Column(length = 2, name = "estado")
      private String uf;
+   
+     private String estadio;
 }
