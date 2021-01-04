a23977cec3d07b2973cfdceb203802b9d9c853ef
Joaquim Silva <joaquim.diego8@gmail.com>
2021-01-04 18:34:23 -0300

    Initial setup

--- Java-Pratica/JAVA/pratica/pratica.java
+++
@@ -1,7 +0,0 @@
-package io.github.jiangdequan;
-
-public class pratica {
-public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    
-}
---
+++ Python/aula2.py
@@ -0,0 +1,22 @@
+a = int(input('Entre com o valor de a: '))
+b = int(input('Entre com valor de b: '))
+print(a)
+print(b)
+
+
+soma= a +b
+subtracao = a-b
+multiplicacao = a*b
+divisao = a/b
+resto = a%b
+
+print('soma: ' + str(soma))
+print (subtracao)
+print (multiplicacao)
+print (int(divisao))
+print(resto)
+
+x= '1'
+
+soma2 = (int(x)+1)
+print (soma2)
