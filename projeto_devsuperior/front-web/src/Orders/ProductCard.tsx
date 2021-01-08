3ed102d5f946f7c7f87f8c400a9beb6602baa859
Joaquim Silva <joaquim.diego8@gmail.com>
2021-01-08 17:32:24 -0300

    Search order location

--- projeto_devsuperior/front-web/src/Orders/ProductCard.tsx
+++ projeto_devsuperior/front-web/src/Orders/ProductCard.tsx
@@ -4,7 +4,6 @@ import { Product } from "./types";
 type Props ={
     product: Product;
 }
-
 function formatPrice(price:  number){
     const formatter = new Intl.NumberFormat('pt-BR', {
         style: 'currency',
