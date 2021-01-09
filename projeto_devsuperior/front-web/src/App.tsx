8922d69b22c1c1188901562f55077957ed6de5a5
Joaquim Silva <joaquim.diego8@gmail.com>
2021-01-08 21:47:52 -0300

    Finish front-web

--- projeto_devsuperior/front-web/src/App.tsx
+++ projeto_devsuperior/front-web/src/App.tsx
@@ -1,4 +1,7 @@
 import './App.css';
+import { ToastContainer} from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 
 import Routes from './Routes';
@@ -7,6 +10,7 @@ function App() {
   return (
     <>
      <Routes />
+     <ToastContainer />
     </>
   );
 }
